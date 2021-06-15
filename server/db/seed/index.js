b221ac8042951b33a6b7096280d1cff458ff8ffe
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-15 17:30:57 -0400

    wrote basic seed functions

---
+++ server/db/seed/index.js
@@ -0,0 +1,43 @@
+const productData=require('./rawProductData')
+const orderData=require("./rawOrderData")
+const userData=require('./rawUserData')
+const {db, User, Product, Order, OrderDetails}=require('../index')
+
+const seed = async () => {
+	try {
+		await db.sync({ force: true });
+		// seed your database here!
+		const products= await Promise.all(
+			productData.map((product) => Product.create(product))
+		);
+		
+		const orders=await Promise.all(orderData.map((order) => Order.create(order)));
+        const users=await Promise.all(
+            userData.map((user)=>User.create(userS))
+        )
+        orders.forEach((order)=>{
+            order.setUser(users[Math.floor(Math.random()*users.length)])
+        })
+        
+	} catch (err) {
+		console.log(red(err));
+	}
+};
+
+module.exports = seed;
+// If this module is being required from another module, then we just export the
+// function, to be used as necessary. But it will run right away if the module
+// is executed directly (e.g. `node seed.js` or `npm run seed`)
+if (require.main === module) {
+	seed()
+		.then(() => {
+			console.log(green("Seeding success!"));
+			db.close();
+		})
+		.catch((err) => {
+			console.error(red("Oh noes! Something went wrong!"));
+			console.error(err);
+			db.close();
+		});
+}
+
