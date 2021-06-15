c92d6b38be150aa1a581084e1d612009a76ef415
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-15 16:18:45 -0400

    feat: Model assosciations

--- server/db/index.js
+++ server/db/index.js
@@ -5,20 +5,29 @@ const db = require('./db');
 const Order=require("./models/orders")
 const User = require('./models/user');
 const Product = require('./models/Product');
-
+const OrderDetails=require('./models/orderDetails')
 
 //associations could go here!
 User.hasMany(Order)
 Order.belongsto(User)
 
-Order.hasMany(Prodcut)
-Product.hasMany(Order)
+Order.belongsToMany(Prodcut,{through: OrderDetails})
+Product.belongsToMany(Order, {through: OrderDetails})
+
+Order.prototype.getTotal=async function(){
+  const productList=await this.getProducts({include:[{model:'orderDetails'}]});
+  
+  productList.reduce((accumulator,product)=>{
+    return accumulator+=product.price*product.orderDetails.quantity
+  }, 0)
+}
 
 module.exports = {
   db,
   models: {
     User,
     Product,
-    Order
+    Order,
+    OrderDetails
   },
 };
