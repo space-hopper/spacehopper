f956afce24a3aa182b86a461f2f1a0952141cc85
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-15 13:03:10 -0400

    feat: Wrote tests for isNull and names on product model

---
+++ tests/models.js
@@ -0,0 +1,64 @@
+const { expect } = require("chai");
+const Product = require("../server/db/models/product");
+const { db, Coffee } = require("../server/models");
+describe("Products model",()=>{
+    beforeEach(() => db.sync({ force: true }));
+    describe("column definitions and validations",()=>{
+        it("has a name, price, quantity, and image URL", async()=>{
+            const coolFrog=await Product.create({
+                name:"Cool Frog",
+                price:68.75,
+                quantity:1,
+                imageUrl:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2jrYIjX0ev9nUN0m19GfAQHaHa%26pid%3DApi&f=1'
+            })
+            expect(coolFrog.name).to.equal("Cool Frog")
+            expect(coolFrog.price).to.equal(68.75)
+            expect(coolFrog.quantity).to.equal(1),
+            expect(coolFrog.imageUrl).to.equal('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2jrYIjX0ev9nUN0m19GfAQHaHa%26pid%3DApi&f=1')
+        })
+        it("name is required",async ()=>{
+            const frog=Product.build({price:68.75,
+                quantity:1,
+                imageUrl:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2jrYIjX0ev9nUN0m19GfAQHaHa%26pid%3DApi&f=1'
+            });
+            return frog.validate().then(
+                ()=>{
+                    throw new Error("Validation should have failed")
+                },
+                (err)=>{
+                    expect(err).to.be.an("error")
+                }
+            )
+        })
+        it("price is required", async ()=>{
+            const frog=Product.build({name:'frog',
+            quantity:1,
+            imageUrl:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2jrYIjX0ev9nUN0m19GfAQHaHa%26pid%3DApi&f=1'
+        });
+        return frog.validate().then(
+            ()=>{
+                throw new Error("Validation should have failed")
+            },
+            (err)=>{
+                expect(err).to.be.an("error")
+            }
+        )
+        })
+        it("quantity is required", async ()=>{
+            const frog=Product.build({
+                name:frog,
+                price:68.75,
+                imageUrl:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2jrYIjX0ev9nUN0m19GfAQHaHa%26pid%3DApi&f=1'
+          
+            })
+            return frog.validate().then(
+                ()=>{
+                    throw new Error("Validation should have failed")
+                },
+                (err)=>{
+                    expect(err).to.be.an("error")
+                }
+            )
+        })
+    })
+})
