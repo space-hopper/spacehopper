4b9e0835761ee6f4bbe609474e6da90db50cee6d
Karen <devkarenc@gmail.com>
2021-06-16 14:44:38 -0400

    Add a route for a specific product by productId

--- server/api/products.js
+++ server/api/products.js
@@ -13,3 +13,14 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
+
+// GET api/products/:productId
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findByPk(productId);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
