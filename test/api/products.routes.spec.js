1a947b9c8a728a10806bbeccf8e02cb5df9453c9
Karen <devkarenc@gmail.com>
2021-06-16 15:45:16 -0400

    Write a test for a single product route

--- test/api/products.routes.spec.js
+++ test/api/products.routes.spec.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
 const request = require('supertest');
-const app = require('../server/app');
-const { Product } = require('../server/db/');
-const seed = require('../../script/seed');
+const app = require('../../server/app');
+const { Product } = require('../../server/db/');
+const seed = require('../../server/db/seed');
 
 describe('Product routes', () => {
   beforeEach(async () => {
@@ -12,9 +12,17 @@ describe('Product routes', () => {
   describe('/api/products/', () => {
     it('GET /api/products', async () => {
       const res = await request(app).get('/api/products').expect(200);
-
+      console.log(res);
       expect(res.body).to.be.an('array');
       expect(res.body.length).to.equal(2);
+    });
+  });
+
+  describe('/api/products/:productId', () => {
+    it('GET /api/products/:productId', async () => {
+      const res = await request(app).get('/api/products/1').expect(200);
+
+      expect(res.body).to.be.an('object');
     });
   });
 });
