c0dddcdebd02c08cf0709e6e84f2e8d08c40c18f
Karen <devkarenc@gmail.com>
2021-06-17 13:09:00 -0400

    doc: add labels to the orders route for documentation purposes

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,42 +1,48 @@
 const router = require('express').Router();
-const {User, Order, Product}=require('../db')
+const { User, Order, Product } = require('../db');
 
 //mounted on /api/orders
-router.post('/:userId', async(req,res,next)=>{
-    try{const order=await Order.create()
-        await order.setUser(await User.findByPk(req.params.userId))
-    res.json(order)}
-    catch(err){
-        next(err)
-    }
-})
-router.get('/cart/:userId', async(req,res,next)=>{
-    try {
-        const order=await Order.findAll({include:{
-            model: User,
-            where:{
-                id:req.params.userId,
-                
-            }
-        },where:{
-            status:"cart"
-        }})
-        res.json(order)
-    } catch (err) {
-        next(err)
-    }
-})
-router.put('/:orderId/:productId', async(req,res,next)=>{
-    try{
-        
-        const product= await Product.findByPk(req.params.productId)
-        const order= await Order.findByPk(req.params.orderId)
-        await order.addProduct(product)
-        res.json(order)
-    }
-    catch(err){
-        next(err)
-    }
-})
+// POST /api/orders/:userId
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const order = await Order.create();
+    await order.setUser(await User.findByPk(req.params.userId));
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports=router
+// GET /api/orders/cart/:userId
+router.get('/cart/:userId', async (req, res, next) => {
+  try {
+    const order = await Order.findAll({
+      include: {
+        model: User,
+        where: {
+          id: req.params.userId,
+        },
+      },
+      where: {
+        status: 'cart',
+      },
+    });
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT /api/orders/:orderId/:productId
+router.put('/:orderId/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId);
+    const order = await Order.findByPk(req.params.orderId);
+    await order.addProduct(product);
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
