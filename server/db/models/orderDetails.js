d84184c2ce19c74473015e96f31c9cd6a6dad47e
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-17 12:10:45 -0400

    commit to get help with debugging

--- server/db/models/orderDetails.js
+++ server/db/models/orderDetails.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const Orders=require('./orders');
+const Product = require('./product');
 
 const OrderDetails=db.define("orderDetails",{
     quantity:{
@@ -8,7 +10,28 @@ const OrderDetails=db.define("orderDetails",{
             validate:{
                 min:0
             }
-        //add pirce
+        
+    },
+    totalPrice:{
+        type:Sequelize.DataTypes.INTEGER,
+        validate:{
+            min:0
+        },
+        get(){
+            return this.getDataValue(totalPrice)/100
+        },
+        set(totalPriceToBeSet){
+            this.setDataValue('totalPrice', Math.floor(totalPriceToBeSet*100))
+        }
     }
 })
+
+
+OrderDetails.beforeValidate=async(orderDetails,options)=>{
+    console.log(orderDetails)
+    const order =await Orders.findByPk(orderDetails.productId)
+    const basePrice=order.price
+    orderDetails.totalPrice=basePrice*orderDetails.quantity
+}
+
 module.exports=OrderDetails
