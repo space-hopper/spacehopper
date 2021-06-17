fbf03d3848bc8d4958926e0f1aaef97ea053d260
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-17 15:40:43 -0400

    pre ticket help commit

--- server/api/orders.js
+++ server/api/orders.js
@@ -26,6 +26,29 @@ router.get('/cart/:userId', async(req,res,next)=>{
         next(err)
     }
 })
+router.put('/checkout/:orderId',async(req,res,next)=>{
+    try{
+        const order = await Order.findByPk(req.params.orderId, {include:{model:'product'}});
+        console.log(order)
+        if(order.status!=='cart') res.status(304).json('This order has already been placed')
+        else{order.setValue('status','pending');
+        const outOfStock=order.products.reduce((accumulator,val)=>{
+            if(val.quantity<=0)accumulator=true
+            return accumulator
+        }, false)
+        if(outOfStock) res.status(304).send('One or more items are out of stock')
+        else{
+            for(let i=0;i<order.products.length;i++){
+                await order.products[i].setValue(order.products[i].quantity-1)
+            }
+            res.send(order)
+        }}
+        
+    }
+    catch(err){
+        next(err)
+    }
+})
 router.put('/:orderId/:productId', async(req,res,next)=>{
     try{
         
