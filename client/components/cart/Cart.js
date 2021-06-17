aeccbfc5ba94a35d46ec32596b9a5c7e5cc48c55
Karen <devkarenc@gmail.com>
2021-06-17 15:45:53 -0400

    feat: set up basic cart page (hold off until user data is available)

--- client/components/cart/Cart.js
+++ client/components/cart/Cart.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchCart } from '../../store/cart';
 import EmptyCartPage from './EmptyCartPage';
 
-const Cart = () => {
-  return <div></div>;
+class Cart extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    // placeholder of 1 for the userId until we have user data
+    this.props.getCart(1);
+  }
+
+  render() {
+    const { cart } = this.props;
+    console.log('cart', cart);
+    return <div>Cart</div>;
+  }
+}
+
+const mapState = (state) => {
+  return {
+    cart: state.cart,
+  };
 };
 
-export default Cart;
+const mapDispatch = (dispatch) => {
+  return {
+    getCart: (userId) => dispatch(fetchCart(userId)),
+  };
+};
+
+export default connect(mapState, mapDispatch)(Cart);
