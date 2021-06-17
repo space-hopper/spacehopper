7b21928c13906d1615c5ca38d64e7301e6e8fbbd
Karen Choi <49035897+DevKarenC@users.noreply.github.com>
2021-06-17 11:47:27 -0400

    Fix user model (#33)
    * chore: set up prettier rc file for consistent code styling
    
    * Allow isMember and isAdmin fields to be null for the User model
    
    * fix: Fix the signup POST route to only take in relevant user data

--- server/auth/index.js
+++ server/auth/index.js
@@ -12,7 +12,13 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body);
+    const userData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const user = await User.create(userData);
     res.send({ token: await user.generateToken() });
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
