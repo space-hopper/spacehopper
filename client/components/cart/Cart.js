273a2911a3f17fcc02c2b8e75db8d45084dad2c1
Karen <devkarenc@gmail.com>
2021-06-17 15:25:02 -0400

    Add action types, creators, thunks, and reducer for set cart

---
+++ client/components/cart/Cart.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EmptyCartPage from './EmptyCartPage';
+
+const Cart = () => {
+  return <div></div>;
+};
+
+export default Cart;
--- client/store/cart.js
+++ client/store/cart.js
@@ -1,13 +1,66 @@
 import axios from 'axios';
 
+// ACTION TYPES
+const SET_CART = 'SET_CART';
+const INCREMENT_QUANTITY = 'INCREMENT_QUANTITY';
+const DECREMENT_QUANTITY = 'DECREMENT_QUANTITY';
+
+// ACTION CREATORS
+const setCart = (cart) => {
+  return {
+    type: SET_CART,
+    cart,
+  };
+};
+
+const incrementQuantity = (productId) => {
+  return {
+    type: INCREMENT_QUANTITY,
+    productId,
+  };
+};
+
+const decrementQuantity = (productId) => {
+  return {
+    type: DECREMENT_QUANTITY,
+    productId,
+  };
+};
+
+// THUNK CREATORS
+export const fetchCart = (userId) => {
+  // THUNK
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/orders/cart/${userId}`);
+      dispatch(setCart(data));
+    } catch (error) {
+      console.log('Error while trying to fetch the cart data!', error);
+    }
+  };
+};
+
+// INITIAL STATE
 const initialState = {
   items: [], //array of objects of each product from db
   addedItems: [], //empty - the user changes this state, adding items to cart
+
+  cart: [],
   total: 0,
 };
 
+// REDUCER
 const cartReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case SET_CART:
+      return action.cart;
+    // case INCREMENT_QUANTITY:
+    //   return ;
+    // case DECREMENT_QUANTITY:
+    //   return
+    default:
+      return state;
+  }
 };
 
 export default cartReducer;
