3311fe74159c7f4243d01881f19190533e360167
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-17 16:41:58 -0400

    added before validate hook

--- server/db/models/orderDetails.js
+++ server/db/models/orderDetails.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-const Orders=require('./orders');
 const Product = require('./product');
 
 const OrderDetails=db.define("orderDetails",{
@@ -19,7 +18,7 @@ const OrderDetails=db.define("orderDetails",{
             min:0
         },
         get(){
-            return this.getDataValue(totalPrice)/100
+            return this.getDataValue('totalPrice')/100
         },
         set(totalPriceToBeSet){
             this.setDataValue('totalPrice', Math.floor(totalPriceToBeSet*100))
@@ -28,11 +27,13 @@ const OrderDetails=db.define("orderDetails",{
 })
 
 
-OrderDetails.beforeCreate(async(orderDetails,options)=>{
-    console.log('hello!!!')
-    const order =await Orders.findByPk(orderDetails.productId)
-    const basePrice=order.price
-    orderDetails.totalPrice=basePrice*orderDetails.quantity
+OrderDetails.beforeValidate(async(orderDetails,options)=>{
+    //if using magic methods, pass
+    //{individualHooks:true}
+    //as the second argument for this to run
+    const product =await Product.findByPk(orderDetails.productId)
+    const basePrice=product.price
+    orderDetails.totalPrice= basePrice*orderDetails.quantity
 })
 
 module.exports=OrderDetails
