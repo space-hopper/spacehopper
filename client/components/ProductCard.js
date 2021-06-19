e2e2a62ddbfb9a364fe923490b3bc004a5e069f4
Karen Choi <49035897+DevKarenC@users.noreply.github.com>
2021-06-19 14:13:17 -0400

    All products page (#43)
    * Add a route to the Products page
    
    * Remove rendering Products component inside of Cart component
    
    * Remove Cart component and add a cart icon to the main sidebar
    
    * Place the sidebar next to the main container
    
    * Create a reusable ProductCard component
    
    * Add grid styling to all products view for organized items
    
    * Add stylings to add to cart button
    
    * Add google fonts and modify font styling for product card
    
    * Add all products heading to the page
    
    * Add routes to the sidebar and trapezoid background to home

---
+++ client/components/ProductCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductCard = ({ item }) => {
+  return (
+    <div className="product-card">
+      <img className="productImage" src={item.imageURL} alt={item.name} />
+      <div className="itemName">{item.name}</div>
+      <div className="productPrice">$ {item.price}</div>
+      <div className="buttonSpacing">
+        <button className="buttonDesign">Add to Cart</button>
+      </div>
+      {/* <button onClick={() => loadItemToCart(item)}>Add To Cart</button> */}
+    </div>
+  );
+};
+
+export default ProductCard;
--- client/components/Products.js
+++ client/components/Products.js
@@ -1,57 +1,45 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import {fetchProducts} from '../redux/actions/ProductThunks'
+import { connect } from 'react-redux';
+import { fetchProducts } from '../redux/actions/ProductThunks';
+import ProductCard from './ProductCard';
 // import { addToCart } from '../redux/actions/CartThunks'
 
 class Products extends React.Component {
-
- componentDidMount(){
-   this.props.loadProducts();
-  //  this.props.loadItemToCart();
- }
+  componentDidMount() {
+    this.props.loadProducts();
+    //  this.props.loadItemToCart();
+  }
 
   render() {
     const products = this.props.products;
-    console.log(products)
+    console.log('products', products);
     return (
-        <>
-        <h1><span className="flipH">𓆏</span> SPACE HOPPER 𓆏 </h1>
-        <h1>THE PLACE TO HOP UP YOUR SPACE</h1>
-
-        {!products ? (
-              <h3>WHERE FOR ART OUR ITEMS</h3>
-          ) : 
-          products.map((item) => {
-            return (
-            <div key={item.id}>
-            <div className="productFormat">
-            <div className="flexBox">
-            <img className="productImage" src={item.imageURL} alt={item.name} />
-            <div className="itemName">{item.name}</div>
-            <div className="productPrice">${item.price}</div>
-            <div className="buttonSpacing">
-            <button className="buttonDesign">Future Button</button>
-            </div>
-            {/* <button onClick={() => loadItemToCart(item)}>Add To Cart</button> */}
-            </div>
-            </div>
-            </div>
-            )
-          })
-        }    
-  </>
- )}
+      <div className="products-page">
+        <p className="page-heading">All Products</p>
+        <div className="products-container">
+          {!products ? (
+            <h3>WHERE FOR ART OUR ITEMS</h3>
+          ) : (
+            products.map((item) => {
+              return <ProductCard key={item.id} item={item} />;
+            })
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => { 
-  console.log('state', state)
-  return ({
-  products: state.productReducer
-})}
+const mapStateToProps = (state) => {
+  console.log('state', state);
+  return {
+    products: state.productReducer,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   loadProducts: () => dispatch(fetchProducts()),
   // loadItemToCart: (id) => dispatch(addToCart(id))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Products)
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
