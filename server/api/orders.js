7aee37da75b135fb9396707e482580dd9c8ad3f4
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-20 00:18:24 -0400

    functional api route, but want to refactor

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,65 +1,83 @@
 const router = require('express').Router();
-const {User, Order, Product}=require('../db')
+const { User, Order, Product, OrderDetails } = require('../db');
 
 //mounted on /api/orders
-router.post('/:userId', async(req,res,next)=>{
-    try{const order=await Order.create()
-        await order.setUser(await User.findByPk(req.params.userId))
-    res.json(order)}
-    catch(err){
-        next(err)
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const order = await Order.create();
+    await order.setUser(await User.findByPk(req.params.userId));
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get('/cart/:userId', async (req, res, next) => {
+  try {
+    const order = await Order.findAll({
+      include: {
+        model: User,
+        where: {
+          id: req.params.userId,
+        },
+      },
+      where: {
+        status: 'cart',
+      },
+    });
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+});
+router.put('/checkout/:orderId', async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.orderId);
+    if (order.status !== 'cart')
+      res.status(403).json('This order has already been placed');
+    else {
+      const products = await order.getProducts();
+      const outOfStock = await products.reduce(async (accumulator, val) => {
+        const orderDetails = await OrderDetails.findOne({
+          where: {
+            productId: val.id,
+            orderId: order.id,
+          },
+        });
+        if (val.quantity < orderDetails.quantity) accumulator = true;
+        return accumulator;
+      }, false);
+      if (outOfStock)
+        res.status(403).json('One or more items are out of stock');
+      else {
+        await order.set('status', 'pending');
+        await order.save();
+        for (let i = 0; i < products.length; i++) {
+          const quantity = products[i].quantity;
+          const orderDetails = await OrderDetails.findOne({
+            where: {
+              productId: products[i].id,
+              orderId: order.id,
+            },
+          });
+          await products[i].set('quantity', quantity - orderDetails.quantity);
+          await products[i].save();
+        }
+        res.send(order);
+      }
     }
-})
-router.get('/cart/:userId', async(req,res,next)=>{
-    try {
-        const order=await Order.findAll({include:{
-            model: User,
-            where:{
-                id:req.params.userId,
-                
-            }
-        },where:{
-            status:"cart"
-        }})
-        res.json(order)
-    } catch (err) {
-        next(err)
-    }
-})
-router.put('/checkout/:orderId',async(req,res,next)=>{
-    try{
-        const order = await Order.findByPk(req.params.orderId, {include:{model:'product'}});
-        console.log(order)
-        if(order.status!=='cart') res.status(304).json('This order has already been placed')
-        else{order.setValue('status','pending');
-        const outOfStock=order.products.reduce((accumulator,val)=>{
-            if(val.quantity<=0)accumulator=true
-            return accumulator
-        }, false)
-        if(outOfStock) res.status(304).send('One or more items are out of stock')
-        else{
-            for(let i=0;i<order.products.length;i++){
-                await order.products[i].setValue(order.products[i].quantity-1)
-            }
-            res.send(order)
-        }}
-        
-    }
-    catch(err){
-        next(err)
-    }
-})
-router.put('/:orderId/:productId', async(req,res,next)=>{
-    try{
-        
-        const product= await Product.findByPk(req.params.productId)
-        const order= await Order.findByPk(req.params.orderId)
-        await order.addProduct(product)
-        res.json(order)
-    }
-    catch(err){
-        next(err)
-    }
-})
+  } catch (err) {
+    next(err);
+  }
+});
+router.put('/:orderId/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId);
+    const order = await Order.findByPk(req.params.orderId);
+    await order.addProduct(product);
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports=router
+module.exports = router;
