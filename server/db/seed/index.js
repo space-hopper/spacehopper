4b0bc1b0bad01815fb35d1f67f287bb551301327
Josephine <josephine.bartholoma@gmail.com>
2021-06-21 17:32:30 -0400

    added frog pics (#57)

--- server/db/seed/index.js
+++ server/db/seed/index.js
@@ -1,30 +1,38 @@
-const productData=require('./rawProductData')
-const orderData=require("./rawOrderData")
-const userData=require('./rawUserData')
-const {db, User, Product, Order, OrderDetails}=require('../index')
+const productData = require('./rawProductData');
+const orderData = require('./rawOrderData');
+const userData = require('./rawUserData');
+const frogImages = require('./rawFrogImages');
+const { db, User, Product, Order } = require('../index');
 
 const seed = async () => {
-	try {
-		await db.sync({ force: true });
-		// seed your database here!
-		const products= await Promise.all(
-			productData.map((product) => Product.create(product))
-		);
-		
-		const orders=await Promise.all(orderData.map((order) => Order.create(order)));
-        const users=await Promise.all(
-            userData.map((user)=>User.create(user))
-        )
-        for(let j=0;j<orders.length;j++){
-			await orders[j].setUser(users[Math.floor(Math.random()*users.length)])
-			for(let i=0;i<Math.ceil(Math.random()*8);i++){
-				await orders[j].addProduct(products[Math.floor(Math.random()*products.length)],{individualHooks:true})
-			}
-		}
-        
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    await db.sync({ force: true });
+    // seed your database here!
+    const products = await Promise.all(
+      productData.map((product) =>
+        Product.create({
+          ...product,
+          imageURL: frogImages[Math.floor(Math.random() * frogImages.length)],
+        }),
+      ),
+    );
+
+    const orders = await Promise.all(
+      orderData.map((order) => Order.create(order)),
+    );
+    const users = await Promise.all(userData.map((user) => User.create(user)));
+    for (let j = 0; j < orders.length; j++) {
+      await orders[j].setUser(users[Math.floor(Math.random() * users.length)]);
+      for (let i = 0; i < Math.ceil(Math.random() * 8); i++) {
+        await orders[j].addProduct(
+          products[Math.floor(Math.random() * products.length)],
+          { individualHooks: true },
+        );
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = seed;
@@ -32,15 +40,14 @@ module.exports = seed;
 // function, to be used as necessary. But it will run right away if the module
 // is executed directly (e.g. `node seed.js` or `npm run seed`)
 if (require.main === module) {
-	seed()
-		.then(() => {
-			console.log("Seeding success!");
-			db.close();
-		})
-		.catch((err) => {
-			console.error("Dang! Something went wrong!");
-			console.error(err);
-			db.close();
-		});
+  seed()
+    .then(() => {
+      console.log('Seeding success!');
+      db.close();
+    })
+    .catch((err) => {
+      console.error('Dang! Something went wrong!');
+      console.error(err);
+      db.close();
+    });
 }
-
