b399a7c983da6defebe121ffb2a186ab247faab4
Karen Choi <49035897+DevKarenC@users.noreply.github.com>
2021-06-20 21:13:10 -0400

    Refactor single product (#45)
    * Add Link to single product page upon clicking on the item name
    
    * Change item name link color to black
    
    * Add styles to the Single Product page
    
    * Change button cursor and add route to /cart

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductCard = ({ item }) => {
   return (
     <div className="product-card">
       <img className="productImage" src={item.imageURL} alt={item.name} />
-      <div className="itemName">{item.name}</div>
+      <Link to={`/products/${item.id}`} className="itemName">
+        {item.name}
+      </Link>
       <div className="productPrice">$ {item.price}</div>
       <div className="buttonSpacing">
         <button className="buttonDesign">Add to Cart</button>
