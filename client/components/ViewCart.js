03543a0f93bd7651242c5f2f38e37251a3f39ded
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-21 17:35:13 -0400

    added $ and fixed decimal place to total

--- client/components/ViewCart.js
+++ client/components/ViewCart.js
@@ -8,28 +8,39 @@ const Cart = (props) => {
     <div>
       {cartItems.map((item) => {
         return (
-        <div className="product-card" key={item.id}>
-          <img className="productImage" src={item.imageURL} alt={item.name} />
-          <div className="itemName">{item.name}</div>
-          <div className="productPrice">$ {item.price}</div>
-          <div className="buttonSpacing"></div>
-          <button className="buttonDesignQ" onClick={() => setValue(value + 1)}>
-            +
-          </button>
-          <input
-            className="quantityButton"
-            onChange={(e) => setValue(e.target.value)}
-            value={value}
-          />
-          <button className="buttonDesignQ" onClick={() => setValue(value - 1)}>
-            -
-          </button>
-        </div>
-        )
+          <div className="product-card" key={item.id}>
+            <img className="productImage" src={item.imageURL} alt={item.name} />
+            <div className="itemName">{item.name}</div>
+            <div className="productPrice">$ {item.price}</div>
+            <div className="buttonSpacing"></div>
+            <button
+              className="buttonDesignQ"
+              onClick={() => setValue(value + 1)}
+            >
+              +
+            </button>
+            <input
+              className="quantityButton"
+              onChange={(e) => setValue(e.target.value)}
+              value={value}
+            />
+            <button
+              className="buttonDesignQ"
+              onClick={() => setValue(value - 1)}
+            >
+              -
+            </button>
+          </div>
+        );
       })}
-      <div>Total: {cartItems.reduce((a, c) => {
-        return a + (c.price * value)
-      }, 0)}</div>
+      <div>
+        Total:{' $'}
+        {cartItems
+          .reduce((a, c) => {
+            return a + c.price * value;
+          }, 0)
+          .toFixed(2)}
+      </div>
     </div>
   );
 };
