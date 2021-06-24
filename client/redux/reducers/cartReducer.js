1c1bd59be56b3c93f2b0c7023e434cf3758c36d9
Josephine <josephine.bartholoma@gmail.com>
2021-06-23 21:12:03 -0400

    Backend cart to frontend (#74)
    * refactored thunk and reducer, but has bug
    
    * fixed the bug
    
    * Hook up front end increment button to back end
    
    * Fix the local count variable being an object
    
    * added delete functionality
    
    * removed extra lines from handleItemDelete
    
    * get cart on loading /cart
    
    * refactored
    
    * changed isLoggedIn to loggedIn to fix bug
    
    Co-authored-by: Karen <devkarenc@gmail.com>

--- client/redux/reducers/cartReducer.js
+++ client/redux/reducers/cartReducer.js
@@ -1,4 +1,9 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, CREATE_ORDER } from '../actions/actions';
+/* eslint-disable no-case-declarations */
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CREATE_ORDER,
+} from '../actions/actions';
 
 const initialState = {
   cartItems: JSON.parse(localStorage.getItem('cartItems') || '[]'),
@@ -7,11 +12,44 @@ const initialState = {
 export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_ORDER:
-      return { ...state, cartItems: action.payload };
+      return { ...state, cartItems: action.products };
     case ADD_TO_CART:
-      return { cartItems: action.payload };
+      if (action.loggedIn) {
+        const newCartItems = action.newItems.map((val) => {
+          let result = { ...val };
+          result.count = val.orderDetails.quantity;
+          return result;
+        });
+        return { ...state, cartItems: newCartItems };
+      } else {
+        // checking if the product already exists in the cart
+        if (
+          state.cartItems.reduce((accumulator, value, i) => {
+            if (value.id == action.newItems[i].id) {
+              accumulator = true;
+            }
+            return accumulator;
+          }, false)
+        ) {
+          const newCartItems = state.cartItems.map((product, i) => {
+            if (product.id == action.newItems[i].id) {
+              product.count++;
+            }
+            return product;
+          });
+          return { ...state, cartItems: newCartItems };
+        } else {
+          return {
+            ...state,
+            cartItems: [...state.cartItems, ...action.newItems],
+          };
+        }
+      }
+
     case REMOVE_FROM_CART:
-      return { cartItems: state.cartItems.filter(item => item !== action.payload) };
+      return {
+        cartItems: state.cartItems.filter((item) => item !== action.payload),
+      };
     default:
       return state;
   }
